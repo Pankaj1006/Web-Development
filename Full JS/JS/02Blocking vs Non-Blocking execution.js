d5ea9070ba158673be3fa8c5df3c74e35bcb7d23
mahendra pankaj <pankajmahendra1006@gmail.com>
2023-03-29 23:25:27 +0530

    Full JS Course

---
+++ Full JS/JS/02Blocking vs Non-Blocking execution.js
@@ -0,0 +1,50 @@
+// Synchronous or blocking
+// - line by line execution
+
+// Asynchronous or non-blocking
+// - line by line execution not guaranteed
+// - callbacks will fire
+
+const fs = require("fs");
+fs.readFile("dele.txt", "utf-8", (err, data)=>{
+    console.log(data);
+});
+console.log("This is a message");
+// note - read node js about 
+const http = require('http');
+const fs = require('fs');\
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const home = fs.readFileSync('index.html')
+const about = fs.readFileSync('./about.html')
+const services = fs.readFileSync('./services.html')
+const contact = fs.readFileSync('./contact.html')
+
+const server = http.createServer((req, res)=>{
+    console.log(req.url);
+    url = req.url;
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    if(url == '/'){
+        res.end(home);
+    }
+    else if(url == '/about'){
+        res.end(about);
+    }
+    else if(url == '/services'){
+        res.end(services);
+    }
+    else if(url == '/contact'){
+        res.end(contact);
+    }
+    else{
+        res.statusCode = 404;
+        res.end("<h1>404 not found</h1>");
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
